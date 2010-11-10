bc3d1984b3de8a2b54938722254107a87167efd5
Pavel Forkert <fxposter@gmail.com>
2010-11-11 00:20:16 +0200

    added :validate => true to translations association

--- lib/simple_model_translations/base.rb
+++ lib/simple_model_translations/base.rb
@@ -50,7 +50,7 @@ module SimpleModelTranslations
         include SimpleModelTranslations::Attributes
         extend SimpleModelTranslations::ClassMethods
         
-        has_many :translations, :class_name => translation_class.name, :dependent => :destroy, :autosave => true
+        has_many :translations, :class_name => translation_class.name, :dependent => :destroy, :autosave => true, :validate => true
         
         if options[:attributes]
           nested_attributes = options[:attributes].is_a?(Hash) ? options[:attributes] : {}
--- lib/simple_model_translations/version.rb
+++ lib/simple_model_translations/version.rb
@@ -1,3 +1,3 @@
 module SimpleModelTranslations
-  VERSION = '0.1.8'
+  VERSION = '0.1.9'
 end
